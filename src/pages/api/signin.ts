7ba6c5b18ebb5b5a947dd4a957e56a92f0fb9cf5
Thiago Almeida <thiago.almeida@elo7.com>
2022-10-03 01:17:01 -0300

    refactoring

--- src/pages/api/signin.ts
+++ src/pages/api/signin.ts
@@ -18,5 +18,5 @@ export default function handler(
       .json({ jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRoaWFnby5hbG1laWRhQGVsbzcuY29tIiwibmFtZSI6IlRoaSBBbG1laWRhIiwicGxhbiI6ImludGVybWVkaWFyeSIsImlhdCI6MTUxNjIzOTAyMn0.kyuh1a5Mg5rqsZV-gIwpHSj1s8cCzSsaKGoQlfnQ16U' });
   }
   return res.status(400)
-    .json({});
+    .json(JSON.parse('{}'));
 }
